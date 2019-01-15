b5c080fca378732c6aa502f43ba4a1fbbb9dd59b
mkaniaa <39928900+mkaniaa@users.noreply.github.com>
2019-01-15 20:26:20 +0100

    builded app

---
+++ build/precache-manifest.7aad2297a77c4a20e2255d98dc5b22d7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "fb9e3b60c88348810e54",
+    "url": "/static/css/main.f6f360ed.chunk.css"
+  },
+  {
+    "revision": "fb9e3b60c88348810e54",
+    "url": "/static/js/main.fb9e3b60.chunk.js"
+  },
+  {
+    "revision": "38c2b1432934c8af2a44",
+    "url": "/static/css/1.7a7860d4.chunk.css"
+  },
+  {
+    "revision": "38c2b1432934c8af2a44",
+    "url": "/static/js/1.38c2b143.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "82088f7fbcd02d05c61b3cb968fe169f",
+    "url": "/index.html"
+  }
+];
